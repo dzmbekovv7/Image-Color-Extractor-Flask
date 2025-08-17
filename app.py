63af5968d2d85d45718d4b0a9c07e6952df9151d
dzmbekovv7 <aziretdzumabekov19@gmail.com>
2025-08-17 13:28:34 +0600

    changes

--- app.py
+++ app.py
@@ -36,4 +36,6 @@ def uploaded_file(filename):
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
